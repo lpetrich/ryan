2f5f0fca08c28bf7b898b23cd26aed2ed71b6011
Ryan Petrich <rpetrich@gmail.com>
2016-05-08 11:33:20 -0600

    Fixed indentation

--- test.c
+++ test.c
@@ -4,17 +4,17 @@
 void printAllCharacters(const char *string)
 {
 	int i=0;
-		while (string[i]!= '\0'){
-			printf("Character: %i\n", string[i]);
-			i++;
-		}
+	while (string[i]!= '\0'){
+		printf("Character: %i\n", string[i]);
+		i++;
+	}
 }
 int main(){
 	char buffer[256];
 	fgets(buffer,12,stdin);
 	printAllCharacters(buffer);
 	if (strcmp(buffer, "Laura\n")==0){
-	puts("Hello");
+		puts("Hello");
 	} else {
 		puts("You suck!");
 	}
